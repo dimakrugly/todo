382db80b1b028668f0f6a9a43421b6c508addb38
dimakrugly <106512918+dimakrugly@users.noreply.github.com>
2022-11-14 19:35:28 +0200

    refactor: build react components

--- src/index.js
+++ src/index.js
@@ -1,17 +1,35 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-
-const el = (
-    <div>
-        <h1>My Todo List</h1>
-        <input placeholder="search"/>
+const  TodoList = () => {
+    return(
         <ul>
             <li>Learn React</li>
             <li>Build awesome app</li>
         </ul>
-    </div>
-);
+    );
+};
+
+const AppHeader = () => {
+    return (<h1>My Todo List</h1>);
+}
+
+const SearchPanel = () => {
+    return  <input placeholder="search"/>;
+};
 
 
-ReactDOM.render(el, document.getElementById('root'))
+const App = () => {
+    return (
+        <div>
+            <AppHeader/>
+            <SearchPanel/>
+            <TodoList/>
+        </div>
+    );
+};
+
+
+
+
+ReactDOM.render(<App/>, document.getElementById('root'));
